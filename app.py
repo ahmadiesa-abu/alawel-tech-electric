1067f5fa7309a9bab55aa0602298eeabc1f0ce9d
abuiesa5 <abuiesa5@192.168.1.149>
2026-03-08 16:04:27 +0200

    change host

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def new():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensure tables are created within the application context
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
